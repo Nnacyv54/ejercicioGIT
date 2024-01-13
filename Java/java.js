2d16e0d3ecf05e6f21168a0bce5614c561ade9a0
Nnacyv54 <nancyvsqz8@gmail.com>
2024-01-12 19:50:37 -0500

    primer commit

---
+++ Java/java.js
@@ -0,0 +1,11 @@
+//[1, 2, true, 66, 55, 44, 33, 'chau', false]
+//Retornar un nuevo array con todos los números pares que contenga el array [1, 2, true, 66, 55, 44, 33, 'chau', false]
+//arr.push('hola')
+
+const arr=[1, 2, true, 66, 55, 44, 33, 'chau', false]
+
+let pares= 
+
+arr.push (Number, hola)
+
+console.log
